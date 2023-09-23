01b2b1033fea091e3956f9b4d0f38ce2e47d6e2d
Abdulloh <12@gmail.com>
2023-09-23 20:10:44 +0500

    aas

---
+++ script.js
@@ -0,0 +1,43 @@
+// 1 :
+
+
+
+// let amount = +prompt('Введите кол-во')
+
+// for(let i = 1; i <= amount; i++) {
+//     if(i == 1) {
+//         console.log(i + ' sheep');
+//     }else {
+//         console.log(i + ' sheeps');
+//     }
+// }
+
+
+// 2 :
+
+
+// let number = +prompt('Введите число')
+// let stepen = +prompt('Введите степень')
+// let answer = 1;
+// for(let i = 0; i < stepen; i++) {
+//     answer *= number
+// }
+ 
+// console.log(answer);
+
+
+// 3 :
+
+// total = +prompt('Введите число');
+// middleSymbol = prompt('Введите символ отступа');
+// lastSymbol = prompt('Введите конечный символ');
+
+// let res = " "; 
+// for (let i = 0; i < total; i++) { 
+//   if(res == 0){ 
+//     res = lastSymbol 
+//   }else { 
+//    res = middleSymbol + res 
+//   } 
+//  console.log(res); 
+// }
